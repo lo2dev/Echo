2b0efa2c6f3b5edde92efa310bed68ef5f33963e
lo <115636509+lo2dev@users.noreply.github.com>
2024-06-24 11:59:08 +0300

    Remove preference action from main.py

--- src/main.py
+++ src/main.py
@@ -33,7 +33,6 @@ class EchoApplication(Adw.Application):
                          flags=Gio.ApplicationFlags.DEFAULT_FLAGS)
         self.create_action('quit', lambda *_: self.quit(), ['<primary>q'])
         self.create_action('about', self.on_about_action)
-        self.create_action('preferences', self.on_preferences_action)
 
     def do_activate(self):
         win = self.props.active_window
@@ -49,18 +48,7 @@ class EchoApplication(Adw.Application):
                                 copyright='© 2024 Lo')
         about.present(self.props.active_window)
 
-    def on_preferences_action(self, widget, _):
-        print('app.preferences action activated')
-
     def create_action(self, name, callback, shortcuts=None):
-        """Add an application action.
-
-        Args:
-            name: the name of the action
-            callback: the function to be called when the action is
-              activated
-            shortcuts: an optional list of accelerators
-        """
         action = Gio.SimpleAction.new(name, None)
         action.connect("activate", callback)
         self.add_action(action)
